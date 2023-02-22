ae7e99befc1f198737734f3f8a185841260f9370
Alfonsina <alfonsina.l@gmail.com>
2023-02-22 18:14:55 -0300

    feat(server): enable all cors requests

--- src/services/server.ts
+++ src/services/server.ts
@@ -1,5 +1,6 @@
 import express from 'express'
 import * as http from 'http'
+import cors from 'cors'
 import router from '@/routes'
 import { errorHandler } from '@/middlewares/errorHandler'
 import { unknownEndpoint } from '@/middlewares/unknownEndpoint'
@@ -8,6 +9,8 @@ const app: express.Application = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+app.use(cors())
 
 app.use('/api', router)
 
